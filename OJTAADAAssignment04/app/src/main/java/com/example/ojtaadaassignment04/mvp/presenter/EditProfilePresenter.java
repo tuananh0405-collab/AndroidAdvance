aae70ba2b70ce37e69f4b32dd2af9ff0ad7bd321
AnhJohnson <vutuananh0405@gmail.com>
2024-11-05 11:09:28 +0700

    Remove unnecessary folders

--- OJTAADAAssignment04/app/src/main/java/com/example/ojtaadaassignment04/mvp/presenter/EditProfilePresenter.java
+++
@@ -1,4 +0,0 @@
-package com.example.ojtaadaassignment04.mvp.presenter;
-
-public class EditProfilePresenter {
-}
--- OJTAADAAssignment04/app/src/main/java/com/example/ojtaadaassignment04/mvp/view/MainView.java
+++
@@ -1,8 +0,0 @@
-package com.example.ojtaadaassignment04.mvp.view;
-
-import com.example.ojtaadaassignment04.mvp.model.User;
-
-public interface MainView {
-    void showUserData(User user);
-    void showErrorMessage(String message);
-}
